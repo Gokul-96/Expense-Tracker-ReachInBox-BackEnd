b7c9a1b8d62f1a625a790225bdb88635626778b1
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-08-18 19:24:29 +0530

    2nd

---
+++ models/Expense.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const ExpenseSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Expense', ExpenseSchema);
